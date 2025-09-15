ac0855e32b71c7efd21c8ea60bfc79b52623a1ab
root <root@USQZNKBLEH.pwcglb.com>
2025-09-15 09:02:23 +0000

    updated code

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "cloudfront_endpoint" {
   value = "https://${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
 }
 
-output "api_gateway_endpoint" {
-  value = aws_api_gateway_stage.api_stage.invoke_url
-}
+# output "api_gateway_endpoint" {
+#   value = aws_api_gateway_stage.api_stage.invoke_url
+# }
 
 output "alb_dns_name" {
   value = "http://${aws_lb.ecs_alb_internal.dns_name}"
