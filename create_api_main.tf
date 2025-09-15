cb8029e8a436b38382c562d009f475bd3d37d2da
root <root@USQZNKBLEH.pwcglb.com>
2025-09-15 09:01:04 +0000

    updated code

--- create_api_main.tf
+++ create_api_main.tf
@@ -6,19 +6,19 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
   binary_media_types = ["image/png", "application/pdf", "application/octet-stream"]
 }
 
-resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [module.create_api]
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  lifecycle {
-    create_before_destroy = true
-  }
-  variables = {
-    deployed_on = formatdate("YYYYMMDD_hh:mm:ss", timestamp())
-  }
-}
+# resource "aws_api_gateway_deployment" "api_deployment" {
+#   depends_on = [module.create_api]
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+#   variables = {
+#     deployed_on = formatdate("YYYYMMDD_hh:mm:ss", timestamp())
+#   }
+# }
 
-resource "aws_api_gateway_stage" "api_stage" {
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name = var.environment
-}
+# resource "aws_api_gateway_stage" "api_stage" {
+#   deployment_id = aws_api_gateway_deployment.api_deployment.id
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   stage_name = var.environment
+# }
