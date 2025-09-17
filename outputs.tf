df5aec7b28664847662e2f837f384320d46f7e9b
root <root@USQZNKBLEH.pwcglb.com>
2025-09-17 08:04:14 +0000

    enabled API gateway

--- outputs.tf
+++ outputs.tf
@@ -6,13 +6,13 @@ output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.cloudfront_distribution.id
 }
 
-# output "api_gateway_endpoint" {
-#   value = aws_api_gateway_stage.api_stage.invoke_url
-# }
+output "api_gateway_endpoint" {
+  value = aws_api_gateway_stage.api_stage.invoke_url
+}
 
-# output "alb_dns_name" {
-#   value = "http://${aws_lb.ecs_alb_internal.dns_name}"
-# }
+output "alb_dns_name" {
+  value = "http://${aws_lb.ecs_alb_internal.dns_name}"
+}
 
 output "static_website_bucket_name" {
   value = aws_s3_bucket.static_website.bucket
