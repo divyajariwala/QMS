3441858c05909f37b6c1114cf448c4ff2741e8b6
root <root@USQZNKBLEH.pwcglb.com>
2025-09-22 14:10:10 +0000

    removed ECS

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,6 @@ output "api_gateway_endpoint" {
   value = aws_api_gateway_stage.api_stage.invoke_url
 }
 
-output "alb_dns_name" {
-  value = "http://${aws_lb.ecs_alb_internal.dns_name}"
-}
-
 output "static_website_bucket_name" {
   value = aws_s3_bucket.static_website.bucket
 }
