70b0b9181b02082141a5d8c073217cf3dbc2ea81
root <root@USQZNKBLEH.pwcglb.com>
2025-10-13 12:10:05 +0000

    kms fix

--- conf/dev/terraform.tfvars
+++ conf/dev/terraform.tfvars
@@ -28,14 +28,6 @@ dynamodb_configs = [
   }
 ]
 lambda_configs = [
-    {
-    function_name = "narrative-extraction-handler"
-    path = "src/app/narrative_extraction_handler"
-    environment_variables = {
-      env = "dev"
-      narrative_metadata_table = "narrative-metadata"
-    }
-  },
   {
     function_name = "narrative-upload-handler"
     path = "src/app/narrative_upload_handler"
@@ -74,7 +66,14 @@ lambda_configs = [
       env = "dev"
       model_id = "arn:aws:bedrock:us-east-1:120569648189:inference-profile/us.anthropic.claude-sonnet-4-20250514-v1:0"
     }
-  }
+  },
+  {
+    function_name = "narrative-extraction-handler"
+    path = "src/app/narrative_extraction_handler"
+    environment_variables = {
+      env = "dev"
+    }
+  },
 ]
 
 
--- create_lambda.tf
+++ create_lambda.tf
@@ -1,21 +1,21 @@
-data "aws_kms_alias" "lambda_default" {
-  name = "alias/aws/lambda"
-}
+# data "aws_kms_alias" "lambda_default" {
+#   name = "alias/aws/lambda"
+# }
 
-module "lambda_functions" {
-  source = "./aws/modules/lambda_function"
-  count = length(var.lambda_configs)
-  short_name = var.short_name
-  environment = var.environment
-  reserved_concurrent_executions = try(var.lambda_configs[count.index].reserved_concurrent_executions, 10)
-  function_name = var.lambda_configs[count.index].function_name
-  environment_variables = var.lambda_configs[count.index].environment_variables
-  role_arn = var.lambda_execution_role_arn
-  file_path = var.lambda_configs[count.index].path
-  security_group_id = var.security_group_id
-  kms_key_arn = data.aws_kms_alias.lambda_default.arn
-  subnet1 = var.subnet1
-  subnet2 = var.subnet2
-  subnet3 = var.subnet3
-  subnet4 = var.subnet4
-}
+# module "lambda_functions" {
+#   source = "./aws/modules/lambda_function"
+#   count = length(var.lambda_configs)
+#   short_name = var.short_name
+#   environment = var.environment
+#   reserved_concurrent_executions = try(var.lambda_configs[count.index].reserved_concurrent_executions, 10)
+#   function_name = var.lambda_configs[count.index].function_name
+#   environment_variables = var.lambda_configs[count.index].environment_variables
+#   role_arn = var.lambda_execution_role_arn
+#   file_path = var.lambda_configs[count.index].path
+#   security_group_id = var.security_group_id
+#   kms_key_arn = data.aws_kms_alias.lambda_default.arn
+#   subnet1 = var.subnet1
+#   subnet2 = var.subnet2
+#   subnet3 = var.subnet3
+#   subnet4 = var.subnet4
+# }
