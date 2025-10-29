a3df26692a3873d20f2226df8477e616c0a08e12
Prasad Deepak Dinesh (US) <prasad.deepak.dinesh+pwcit@pwc.com>
2025-10-29 11:20:38 +0530

    minor styling changes

--- src/ui/src/components/complaint/ComplaintsFilter.tsx
+++ src/ui/src/components/complaint/ComplaintsFilter.tsx
@@ -1,30 +1,16 @@
 import React from "react";
+import Search from "../../assets/icons/search.svg";
 import styles from "./ComplaintsFilter.module.scss";
 
 const ComplaintsFilter: React.FC = () => {
-  const SearchSvg = () => (
-    <svg
-      className={styles.searchIcon}
-      width="16"
-      height="16"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      focusable="false"
-    >
-      <circle cx="11" cy="11" r="7" />
-      <line x1="21" y1="21" x2="16.65" y2="16.65" />
-    </svg>
-  );
+const SearchSvg = () => <img src={Search} alt="Search icon" />;
 
   return (
     <div className={styles.searchBar}>
       <div className={styles.inputWrapper}>
-        {SearchSvg()}
+        <span className={styles.searchIcon}>
+          <SearchSvg />
+        </span>
         <input
           type="search"
           placeholder="Search here..."
@@ -36,7 +22,9 @@ const ComplaintsFilter: React.FC = () => {
           className={styles.searchButtonInside}
           aria-label="Search Button"
         >
-          <span style={{position: 'absolute', right: 115, bottom: 27}}>{SearchSvg()}</span>
+          <span className={styles.searchButtonIcon}>
+            <SearchSvg />
+          </span>
           <span className={styles.searchButtonText}>Search</span>
         </button>
       </div>
