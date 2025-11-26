932a3c309a1dc8b98be14ed43d5502ef4b7bd787
Anirudh Gunda <anirudh.gunda@pwc.com>
2025-11-26 07:42:45 +0000

    added custom error response

--- create_cloudfront.tf
+++ create_cloudfront.tf
@@ -28,6 +28,18 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     default_ttl                = 0
     max_ttl                    = 0
   }
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
   viewer_certificate {
     cloudfront_default_certificate = true
   }
